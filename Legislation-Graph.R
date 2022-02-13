2de39404a26f57da29c869ac11b9f134e063bf7f
Connor McNamee <mcnameco@mail.gvsu.edu>
2022-02-13 02:35:20 -0500

    Created data

--- Legislation-Graph.R
+++ Legislation-Graph.R
@@ -2,8 +2,29 @@ library(tidyverse)
 library(circlize)
 
 Legislation_Data <- read_csv("Legislation-Data.csv")
-  
 
+State <- rep(c("Alabama","Arkansas","Connecticut","Delaware","Florida",
+           "Georgia","Illinois","Indiana","Iowa","Kentucky","Louisiana",
+           "Maine","Maryland","Massachusetts","Michigan","Minnesota",
+           "Mississippi","Missouri","Nebraska","New Hampshire","New Jersey",
+           "New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Pennsylvania",
+           "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Vermont","Virginia",
+           "West Virginia","Wisconsin","Nevada......1914","Montana.....1914","Kansas.....1912",
+           "Arizona.....1912","Oregon.....1912","California.....1911","Washington.....1910",
+           "Utah......1896","Idaho.....1896","Colorado.....1893","Wyoming......1869"),times = 10)
+Fill <- c(1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0,0,0,1,1,1,1,
+          0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,0,0,1,0,0,0,0,0,0,1,0,0,1,
+          0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
+          1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,0,1,1,1,0,1,1,0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,
+          1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,1,0,1,
+          1,0,1,0,1,1,0,1,0,1,1,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
+          1,1,0,1,1,1,0,1,0,0,1,0,1,0,1,0,1,0,0,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,0,
+          1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,0,0,1,0,1,0,0,0,0,
+          1,1,1,1,1,1,0,0,0,1,1,0,1,0,0,0,1,1,0,1,0,1,0,1,0,1,1,0,1,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,
+          0,0,1,1,1,0,1,0,0,1,1,0,1,1,0,1,0,1,0,0,1,0,1,0,0,1,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1
+         )
+Legislation <- rep(paste("Leg_", 1:10, sep=""),each = 48)
+State_Legislation <- tibble(State,Legislation,Fill)
   
 
 circos.initialize(factors = c(1:48), xlim = c(-5,5))
@@ -30,6 +51,5 @@ circos.trackPlotRegion(ylim = c(0, 1), factors = c(1:48), bg.col = "black",
                        track.height = 0.05)
 circos.clear()
 
-
-
-?circos.par
+ggplot(Legislation_Data) +
+  geom_tile(aes(x = State, y = Legislation, fill = Fill))
